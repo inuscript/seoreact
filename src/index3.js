3c62628eb1cb26ccba21c6dd6f10bbbdf0c674dd
inuscript <inuscript@gmail.com>
2016-04-03 23:21:27 +0900

    timeout=1000

--- src/index3.js
+++ src/index3.js
@@ -4,4 +4,4 @@ var ReactDOM = require('react-dom')
 var hello = React.createElement('div', null, 'hello')
 setTimeout(function(){
   ReactDOM.render(hello, document.getElementById('container'))
-}, 0)
+}, 1000)
