7af6790d5c7fde415f78f7a185baed1cebb46c2d
inuscript <inuscript@gmail.com>
2016-04-03 23:09:13 +0900

    add2

---
+++ src/index2.js
@@ -0,0 +1,12 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+function createItem(i){
+  return React.createElement('div', null, i)
+}
+var childs = []
+for(i = 0; i < 1000; i++){
+  childs.push(createItem(i))
+}
+var hello = React.createElement('div', null, childs)
+ReactDOM.render(hello, document.getElementById('container'))
