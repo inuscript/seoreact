80e18706b5398d77cf9cce18f960750ff61e4d4d
inuscript <inuscript@gmail.com>
2016-04-04 23:05:45 +0900

    arrayk

---
+++ src/arraykeys.js
@@ -0,0 +1,15 @@
+console.time('start')
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+// var arr = Array.apply(null, Array(5)).map(function (_, i) {return i;}).join(',')
+var arr = Array.from([1,2,4]).map(function (_, i) {
+  console.log(i)
+  return i;
+}).join(',')
+console.log(arr)
+var hello = React.createElement('div', null, arr)
+ReactDOM.render(hello, document.getElementById('container'), function(){
+  console.timeEnd('start')
+  
+})
