70e53facf6c1b09a1841e6280ae49e16146a3d44
inuscript <inuscript@gmail.com>
2016-04-04 20:26:05 +0900

    add

--- src/arrayfill.js
+++ src/arrayfill.js
@@ -1,9 +1,10 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var arr = new Array(5).fill('a')
-// .map((item, i) => {
-//   return i
-// })
+.map((item, i) => {
+  return i
+})
+var arr = [1,2,3]
 // console.log(arr)
 var hello = React.createElement('div', null, arr.join(','))
 ReactDOM.render(hello, document.getElementById('container'))
