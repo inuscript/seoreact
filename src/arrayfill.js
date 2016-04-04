2b08b4fada7db1f27631f4fdd9cea6c8ebb32d5b
inuscript <inuscript@gmail.com>
2016-04-04 20:12:41 +0900

    array fill

---
+++ src/arrayfill.js
@@ -0,0 +1,5 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var hello = React.createElement('div', null, 'hello')
+ReactDOM.render(hello, document.getElementById('container'))
--- src/index2.js
+++ src/index2.js
@@ -1,14 +1,17 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var qs = require('querystring')
-
+console.time('app')
 function createItem(i){
   return React.createElement('div', null, i)
 }
 
 var childs = []
-for(i = 0; i < 100000; i++){
+for(i = 0; i < 10000; i++){
   childs.push(createItem(i))
 }
 var hello = React.createElement('div', null, childs)
-ReactDOM.render(hello, document.getElementById('container'))
+ReactDOM.render(hello, document.getElementById('container'), function(){
+  console.timeEnd('app')
+
+})
